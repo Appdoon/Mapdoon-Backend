7608ed5ef4d07dea97ec84d720b08e4aa6c7de63
MrSekiro <ab.arshia@gmail.com>
2023-10-31 13:05:54 +0330

    Create register api based on jwt token

--- Src/Appdoon.WebApi/Controllers/JWTAuthenticationController.cs
+++ Src/Appdoon.WebApi/Controllers/JWTAuthenticationController.cs
@@ -1,7 +1,13 @@
 ﻿using Appdoon.Application.Services.Users.Command.LoginUserService;
+using Appdoon.Application.Services.Users.Command.RegisterUserService;
 using Appdoon.Common.Dtos;
 using Mapdoon.Application.Services.JWTAuthentication.Command;
+using Mapdoon.Common.User;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Mapdoon.WebApi.Controllers
@@ -36,6 +42,33 @@ namespace Mapdoon.WebApi.Controllers
 			}
 
 			return new JsonResult(userResult);
+		}
+
+		[HttpPost]
+		public JsonResult Register([FromServices] IRegisterUserService registerUserService, [FromServices] IJWTProvider jwtProvider, RequestRegisterUserDto user)
+		{
+			// use new regiser user service
+			var registerResult = registerUserService.Execute(user);
+
+			if(registerResult.IsSuccess == true)
+			{
+				var token = jwtProvider.Generate(new UserLoginInfoDto() 
+				{ 
+					Id = registerResult.Data.Id,
+					Email = registerResult.Data.Email,
+					Username = registerResult.Data.Username,
+				});
+
+				var result = new ResultDto<string>()
+				{
+					Data = token,
+					IsSuccess = true
+				};
+
+				return new JsonResult(result);
+			}
+
+			return new JsonResult(registerResult);
 		}
 	}
 }
