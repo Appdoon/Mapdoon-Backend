21b3d80a41bdef7e91df59005dfbaddb0ab26132
Arman <Armansinaie1380@gmail.com>
2023-11-17 23:19:20 +0330

    fix: Some Refactoring...

--- Tests/Mapdoon.Application.Tests/Testing.cs
+++ Tests/Mapdoon.Application.Tests/Testing.cs
@@ -24,29 +24,6 @@ namespace Mapdoon.Application.Tests
         [OneTimeSetUp]
         public async Task RunBeforeAnyTestsAsync()
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true)
-                .AddEnvironmentVariables();
-
-            _configuration = builder.Build();
-
-            var services = new ServiceCollection();
-            var startup = new Startup(_configuration);
-
-            services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
-            w.ApplicationName == "Mapdoon.WebApi" &&
-            w.EnvironmentName == "Development"));
-
-            startup.ConfigureServices(services);
-            _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
-
-            _connectionString = _configuration.GetConnectionString("OUAppCon");
-
-            _respawner = await Respawner.CreateAsync(_connectionString, new RespawnerOptions
-            {
-                TablesToIgnore = new Table[]{ "__EFMigrationsHistory" },
-            });
         }
 
         public static async Task ResetStateAsync()
