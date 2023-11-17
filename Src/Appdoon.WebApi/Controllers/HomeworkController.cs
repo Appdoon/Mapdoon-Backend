a15ed99ce70ab80d95f611710c431715a115dc31
MrSekiro <ab.arshia@gmail.com>
2023-11-17 18:17:25 +0330

    Add api for get specific user homework submission

--- Src/Appdoon.WebApi/Controllers/HomeworkController.cs
+++ Src/Appdoon.WebApi/Controllers/HomeworkController.cs
@@ -15,121 +15,130 @@ using System.Threading.Tasks;
 
 namespace Appdoon.WebApi.Controllers
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class HomeworkController : ControllerBase
-    {
-        //Get
-        private readonly IGetHomeworkService _getHomeworkService;
-        //Create
-        private readonly ICreateHomeworkService _createHomeworkService;
-        //Update
-        private readonly IUpdateHomeworkService _updateHomeworkService;
-        //Delete
-        private readonly IDeleteHomeworkService _deleteHomeworkService;
-        //Get All
-        private readonly IGetAllHomeworksService _getAllHomeworksService;
-        // Get by creatorId
-        private readonly IGetHomeworksByCreator _getHomeworksByCreator;
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class HomeworkController : ControllerBase
+	{
+		//Get
+		private readonly IGetHomeworkService _getHomeworkService;
+		//Create
+		private readonly ICreateHomeworkService _createHomeworkService;
+		//Update
+		private readonly IUpdateHomeworkService _updateHomeworkService;
+		//Delete
+		private readonly IDeleteHomeworkService _deleteHomeworkService;
+		//Get All
+		private readonly IGetAllHomeworksService _getAllHomeworksService;
+		// Get by creatorId
+		private readonly IGetHomeworksByCreator _getHomeworksByCreator;
 
-        private readonly ICurrentContext _currentContext;
+		private readonly ICurrentContext _currentContext;
 
-        private readonly IWebHostEnvironment _env;
+		private readonly IWebHostEnvironment _env;
 
-        public HomeworkController(IGetHomeworkService getHomeworkService,
-                                  ICreateHomeworkService createHomeworkService,
-                                  IUpdateHomeworkService updateHomeworkService,
-                                  IDeleteHomeworkService deleteHomeworkService,
-                                  IGetAllHomeworksService getAllHomeworksService,
-                                  IGetHomeworksByCreator getHomeworksByCreator,
-                                  ICurrentContext currentContext,
-                                  IWebHostEnvironment env)
-        {
-            _getHomeworkService = getHomeworkService;
-            _createHomeworkService = createHomeworkService;
-            _updateHomeworkService = updateHomeworkService;
-            _deleteHomeworkService = deleteHomeworkService;
-            _getAllHomeworksService = getAllHomeworksService;
-            _getHomeworksByCreator = getHomeworksByCreator;
-            _currentContext = currentContext;
-            _env = env;
-        }
+		public HomeworkController(IGetHomeworkService getHomeworkService,
+								  ICreateHomeworkService createHomeworkService,
+								  IUpdateHomeworkService updateHomeworkService,
+								  IDeleteHomeworkService deleteHomeworkService,
+								  IGetAllHomeworksService getAllHomeworksService,
+								  IGetHomeworksByCreator getHomeworksByCreator,
+								  ICurrentContext currentContext,
+								  IWebHostEnvironment env)
+		{
+			_getHomeworkService = getHomeworkService;
+			_createHomeworkService = createHomeworkService;
+			_updateHomeworkService = updateHomeworkService;
+			_deleteHomeworkService = deleteHomeworkService;
+			_getAllHomeworksService = getAllHomeworksService;
+			_getHomeworksByCreator = getHomeworksByCreator;
+			_currentContext = currentContext;
+			_env = env;
+		}
 
-        [HttpGet]
-        public JsonResult Get(int PageNumber = 1, int PageSize = 15)
-        {
-            var result = _getAllHomeworksService.Execute(PageNumber, PageSize);
-            return new JsonResult(result);
-        }
+		[HttpGet]
+		public JsonResult Get(int PageNumber = 1, int PageSize = 15)
+		{
+			var result = _getAllHomeworksService.Execute(PageNumber, PageSize);
+			return new JsonResult(result);
+		}
 
-        [HttpGet("{id}")]
-        public JsonResult Get(int id)
-        {
-            var result = _getHomeworkService.Execute(id);
+		[HttpGet("{id}")]
+		public JsonResult Get(int id)
+		{
+			var result = _getHomeworkService.Execute(id);
 
-            return new JsonResult(result);
-        }
-        [HttpGet("{creatorId}")]
-        public JsonResult GetByCreatorId(int creatorId)
-        {
-            var result = _getHomeworksByCreator.Execute(creatorId);
+			return new JsonResult(result);
+		}
+		[HttpGet("{creatorId}")]
+		public JsonResult GetByCreatorId(int creatorId)
+		{
+			var result = _getHomeworksByCreator.Execute(creatorId);
 
-            return new JsonResult(result);
-        }
+			return new JsonResult(result);
+		}
 
-        [HttpGet]
-        [Authorize(policy: "User")]
-        public async Task<JsonResult> GetSubmission([FromServices] IGetHomeworkSubmissionsService getHomeworkSubmissionsService, GetUserSubmissionDto getUserSubmissionDto)
-        {
+		[HttpGet]
+		[Authorize(policy: "User")]
+		public async Task<JsonResult> GetSubmission([FromServices] IGetHomeworkSubmissionsService getHomeworkSubmissionsService, GetUserSubmissionDto getUserSubmissionDto)
+		{
 			var result = await getHomeworkSubmissionsService.GetUserSubmission(getUserSubmissionDto, _currentContext.User.Id);
 
 			return new JsonResult(result);
 		}
 
-        [HttpPost]
-        [Authorize(policy: "User")]
-        public async Task<JsonResult> SubmitHomework([FromServices] ISubmitHomeworkService submitHomeworkService, SubmitHomeworkDto submitHomeworkDto)
-        {
-            var result = await submitHomeworkService.SubmitHomework(submitHomeworkDto, _currentContext.User.Id);
+		[HttpGet]
+		[Authorize(policy: "User")]
+		public async Task<JsonResult> GetSubmissionForSpecficUser([FromServices] IGetHomeworkSubmissionsService getHomeworkSubmissionsService, int homeworkId, int UserId)
+		{
+			var result = await getHomeworkSubmissionsService.GetUserSubmission(new GetUserSubmissionDto() { HoemworkId = homeworkId }, UserId);
 
-            return new JsonResult(result);
-        }
+			return new JsonResult(result);
+		}
 
-        [HttpPut]
-        [Authorize(policy: "User")]
-        public async Task<JsonResult> EditSubmission([FromServices] IEditHomeworkSubmissionService editHomeworkSubmissionService, EditHomeworkSubmissionDto editHomeworkSubmissionDto)
-        {
+		[HttpPost]
+		[Authorize(policy: "User")]
+		public async Task<JsonResult> SubmitHomework([FromServices] ISubmitHomeworkService submitHomeworkService, SubmitHomeworkDto submitHomeworkDto)
+		{
+			var result = await submitHomeworkService.SubmitHomework(submitHomeworkDto, _currentContext.User.Id);
+
+			return new JsonResult(result);
+		}
+
+		[HttpPut]
+		[Authorize(policy: "User")]
+		public async Task<JsonResult> EditSubmission([FromServices] IEditHomeworkSubmissionService editHomeworkSubmissionService, EditHomeworkSubmissionDto editHomeworkSubmissionDto)
+		{
 			var result = await editHomeworkSubmissionService.EditSubmission(editHomeworkSubmissionDto, _currentContext.User.Id);
 
 			return new JsonResult(result);
 		}
 
-        [HttpPost]
-        public JsonResult Post(CreateHomeworkDto homeworkDto)
-        {
-            int userId = GetIdFromCookie();
-            var result = _createHomeworkService.Execute(homeworkDto, userId);
-            return new JsonResult(result);
-        }
+		[HttpPost]
+		public JsonResult Post(CreateHomeworkDto homeworkDto)
+		{
+			int userId = GetIdFromCookie();
+			var result = _createHomeworkService.Execute(homeworkDto, userId);
+			return new JsonResult(result);
+		}
 
-        [HttpPut("{id}")]
-        public JsonResult Put(int id, UpdateHomeworkDto updateHomeworkDto)
-        {
-            var result = _updateHomeworkService.Execute(id, updateHomeworkDto);
-            return new JsonResult(result);
-        }
+		[HttpPut("{id}")]
+		public JsonResult Put(int id, UpdateHomeworkDto updateHomeworkDto)
+		{
+			var result = _updateHomeworkService.Execute(id, updateHomeworkDto);
+			return new JsonResult(result);
+		}
 
-        [HttpDelete("{id}")]
-        public JsonResult Delete(int id)
-        {
-            var result = _deleteHomeworkService.Execute(id);
-            return new JsonResult(result);
-        }
+		[HttpDelete("{id}")]
+		public JsonResult Delete(int id)
+		{
+			var result = _deleteHomeworkService.Execute(id);
+			return new JsonResult(result);
+		}
 
-        private int GetIdFromCookie()
+		private int GetIdFromCookie()
 		{
 			var user = _currentContext.User;
 			return user.Id;
 		}
-    }
+	}
 }
