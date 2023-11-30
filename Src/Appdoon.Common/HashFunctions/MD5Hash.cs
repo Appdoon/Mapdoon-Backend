96c6205f7972d555441e7cfecfeb93f010e464f3
Arman <Armansinaie1380@gmail.com>
2023-11-30 16:09:00 +0330

    feat: add md5 hash method

---
+++ Src/Appdoon.Common/HashFunctions/MD5Hash.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapdoon.Common.HashFunctions
+{
+    public class MD5Hash
+    {
+        public static string ComputeMD5(string s)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hashValue = md5.ComputeHash(Encoding.UTF8.GetBytes(s));
+                return Convert.ToHexString(hashValue);
+            }
+        }
+    }
+}
