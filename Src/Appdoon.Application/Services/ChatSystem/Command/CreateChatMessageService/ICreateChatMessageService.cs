3f278f9f314cfc4fafa1bd5713e3e97780b45fd6
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-12-11 23:27:00 +0330

    feat : CRUD chat-system

---
+++ Src/Appdoon.Application/Services/ChatSystem/Command/CreateChatMessageService/ICreateChatMessageService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appdoon.Application.Interfaces;
+using Appdoon.Common.Dtos;
+using Mapdoon.Application.Services.Comments.Command.CreateCommentService;
+using Mapdoon.Common.Interfaces;
+using Mapdoon.Domain.Entities.Chat;
+
+namespace Mapdoon.Application.Services.ChatSystem.Command.CreateChatMessageService
+{
+    public class CreateMessageDto
+    {
+        public string Message { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? ReplyMessageId { get; set; }
+    }
+    public interface ICreateChatMessageService : ITransientService
+    {
+        ResultDto Execute(int roadmapId, int userId, CreateMessageDto message);
+    }
+    public class CreateChatMessageService : ICreateChatMessageService
+    {
+        private readonly IDatabaseContext _context;
+        public CreateChatMessageService(IDatabaseContext context)
+        {
+            this._context = context;
+        }
+        public ResultDto Execute(int roadmapId, int userId, CreateMessageDto message)
+        {
+            try
+            {
+                ChatMessage chatmessage = new ChatMessage()
+                {
+                    Message = message.Message,
+                    ImageUrl = message.ImageUrl,
+                    ReplyMessageId = message.ReplyMessageId,
+                    SenderId = userId,
+                    RoadMapId = roadmapId
+                };
+                _context.ChatMessages.Add(chatmessage);
+                _context.SaveChanges();
+                return new ResultDto()
+                {
+                    IsSuccess = true,
+                    Message = "پیام شما با موفقیت ارسال شد.",
+                };
+            }
+            catch (Exception e)
+            {
+                return new ResultDto()
+                {
+                    IsSuccess = false,
+                    Message = e.Message,
+                };
+            }
+        }
+    }
+}
---
+++ Src/Appdoon.WebApi/Controllers/ChatSystemController.cs
@@ -0,0 +1,42 @@
+﻿using Mapdoon.Application.Services.ChatSystem.Command.CreateChatMessageService;
+using Mapdoon.Application.Services.ChatSystem.Query.GetAllMessagesService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using Mapdoon.Application.Services.JWTAuthentication.Command;
+using Mapdoon.Common.Interfaces;
+
+
+namespace Mapdoon.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ChatSystemController : ControllerBase
+    {
+        private readonly ICreateChatMessageService _createChatMessageService;
+        private readonly IGetAllMessagesService _getallmessageservice;
+        public ChatSystemController(ICreateChatMessageService createChatMessageService
+                                    , IGetAllMessagesService getallmessageservice)
+        {
+            _createChatMessageService = createChatMessageService;
+            _getallmessageservice = getallmessageservice;
+        }
+        [HttpPost("{RoadMapId}")]
+        public IActionResult Post(int RoadMapId, CreateMessageDto message,[FromServices] ICurrentContext currentContext)
+        {
+            int userId = currentContext.User.Id;
+            var result = _createChatMessageService.Execute(RoadMapId , userId , message);
+            return Ok(result);
+        }
+        [HttpGet("{RoadMapId}")]
+        public IActionResult Get(int RoadMapId , int PageNumber = 1, int PageSize = 15)
+        {
+            var result = _getallmessageservice.Execute(RoadMapId , PageNumber , PageSize);
+            return Ok(result);
+        }
+    }
+}
