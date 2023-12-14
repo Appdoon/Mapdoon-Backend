1dd4db7e5d54eb1bd2c05c2fa2d33ab1b72f58dc
Arman <Armansinaie1380@gmail.com>
2023-12-14 21:36:04 +0330

    feat: add tests for landing page services

---
+++ Tests/Mapdoon.Application.Tests/LandingPage/Queries/GetStatisticsTests.cs
@@ -0,0 +1,50 @@
+﻿using Appdoon.Application.Services.Homeworks.Query.GetAllHomeworksService;
+using Appdoon.Domain.Entities.HomeWorks;
+using Appdoon.Domain.Entities.RoadMaps;
+using FluentAssertions;
+
+namespace Mapdoon.Application.Tests.LandingPage.Queries
+{
+    using static Testing;
+    internal class GetStatisticsTests : TestBase
+    {
+        [Test]
+        public void ShouldGetStatistics()
+        {
+            int userId = AddUser();
+
+            int roadmapId = AddEntity(new RoadMap
+            {
+                CreatoreId = userId,
+            });
+
+            int stepId = AddEntity(new Step()
+            {
+                RoadMapId = roadmapId
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                int homeworkId = AddEntity(new Homework
+                {
+                    Title = "Title",
+                    Question = "Question",
+                    MinScore = 1,
+                    CreatorId = userId,
+                });
+
+                int childStepId = AddEntity(new ChildStep
+                {
+                    StepId = stepId,
+                    HomeworkId = homeworkId,
+                });
+
+                GetDatabaseContext().SaveChanges();
+            }
+
+            var result = new GetAllHomeworksService(GetDatabaseContext()).Execute();
+            result.IsSuccess.Should().BeTrue();
+            result.Data.Homeworks.Should().HaveCount(10);
+        }
+    }
+}
---
+++ Tests/Mapdoon.Application.Tests/LandingPage/Queries/GetTopEnrolledRoadmapsTests.cs
@@ -0,0 +1,40 @@
+﻿using Appdoon.Domain.Entities.HomeWorks;
+using Appdoon.Domain.Entities.Users;
+using FluentAssertions;
+using Mapdoon.Application.Services.Homeworks.Query.GetHomeworkSubmissions;
+
+namespace Mapdoon.Application.Tests.LandingPage.Queries
+{
+    using static Testing;
+    internal class TopEnrolledRoadmapsTests : TestBase
+    {
+        [Test]
+        public async Task ShouldTopEnrolledRoadmapsTAsync()
+        {
+            var userId = AddEntity(new User
+            {
+                Email = "test@gmail.com",
+                Password = "password",
+            });
+
+            var homeworkId = AddEntity(new Homework
+            {
+                Title = "Title",
+                Question = "Question",
+                CreatorId = userId,
+            });
+
+            var submitHomeworkDto = new GetUserSubmissionDto
+            {
+                HoemworkId = homeworkId,
+            };
+
+            // Act
+            var result = await new GetHomeworkSubmissionsService(GetDatabaseContext()).GetUserSubmission(submitHomeworkDto, userId);
+
+            // Assert
+            result.IsSuccess.Should().Be(true);
+            result.Data.Should().BeNull();
+        }
+    }
+}
