1505876480971d955f45f1930bbe11783f1984f0
MrSekiro <ab.arshia@gmail.com>
2023-12-25 23:57:23 +0330

    fix: solve the issue about the authentication for Hub

--- Src/Appdoon.WebApi/WebSocket/Hubs/MapdoonHub.cs
+++ Src/Appdoon.WebApi/WebSocket/Hubs/MapdoonHub.cs
@@ -4,45 +4,43 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Mapdoon.WebApi.WebSocket.Hubs
 {
+	[Authorize]
 	public class MapdoonHub : Hub
 	{
 		private readonly IUserHubConnectionIdManager _connectionIdManager;
 		private readonly IHttpContextAccessor _httpContextAccessor;
-		private readonly ICurrentContext _currentContext;
 
 		public MapdoonHub(IUserHubConnectionIdManager connectionIdManager,
-			IHttpContextAccessor httpContextAccessor,
-			ICurrentContext currentContext)
+			IHttpContextAccessor httpContextAccessor)
 		{
 			_connectionIdManager = connectionIdManager;
 			_httpContextAccessor = httpContextAccessor;
-			_currentContext = currentContext;
 		}
 		public override Task OnConnectedAsync()
 		{
 			//var token = _httpContextAccessor.HttpContext.Request?.Headers["Authorization"].ToString().Replace("Bearer ", "");
 
-			var user = Context.User;
+			var userId = Context.User.FindFirstValue("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
 
-			if(_currentContext.User.Id != 0)
-				SaveUserHubConnectionId(Context.ConnectionId);
+			if(userId != null)
+				SaveUserHubConnectionId(Context.ConnectionId, userId);
 			return base.OnConnectedAsync();
 		}
 
 		public override Task OnDisconnectedAsync(Exception exception)
 		{
-			var context = Context.GetHttpContext();
-			var userClaim = context.User.Claims
-				.FirstOrDefault(x => x.Type == "sub");
+			var userId = Context.User.FindFirstValue("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
 
-			if(userClaim != null)
+			if(userId != null)
 			{
-				_connectionIdManager.Remove(userClaim.Value, Context.ConnectionId);
+				_connectionIdManager.Remove(userId, Context.ConnectionId);
 			}
 
 			//var userId = _currentContext.User?.Id;
@@ -54,11 +52,11 @@ namespace Mapdoon.WebApi.WebSocket.Hubs
 			return base.OnDisconnectedAsync(exception);
 		}
 
-		private void SaveUserHubConnectionId(string connectionId)
+		private void SaveUserHubConnectionId(string connectionId, string userId)
 		{
 			if(string.IsNullOrWhiteSpace(connectionId) == false)
 			{
-				_connectionIdManager.Add(_currentContext.User.Id.ToString(), connectionId);
+				_connectionIdManager.Add(userId, connectionId);
 			}
 		}
 	}
