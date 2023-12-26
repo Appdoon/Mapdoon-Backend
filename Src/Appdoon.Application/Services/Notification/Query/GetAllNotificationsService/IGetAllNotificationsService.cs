e36b8838a27ea6b5354974efa09e8951b25b87f4
MrSekiro <ab.arshia@gmail.com>
2023-12-26 18:30:46 +0330

    feat: add apis for notification

---
+++ Src/Appdoon.Application/Services/Notification/Query/GetAllNotificationsService/IGetAllNotificationsService.cs
@@ -0,0 +1,70 @@
+﻿using Appdoon.Application.Interfaces;
+using Appdoon.Common.Dtos;
+using Mapdoon.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mapdoon.Application.Services.Notification.Query.GetAllNotificationsService
+{
+	public interface IGetAllNotificationsService : ITransientService
+	{
+		Task<ResultDto<List<GetNotificationResultDto>>> GetAllNotifications(GetNotificationDto input, int userId);
+	}
+
+	public class GetNotificationDto
+	{
+		public bool SeenNotificationsOnly { get; set; } = false;
+    }
+
+	public class GetNotificationResultDto
+	{
+		public int NotificationId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+	public class GetAllNotificationsService : IGetAllNotificationsService
+	{
+		private readonly IDatabaseContext _databaseContext;
+
+		public GetAllNotificationsService(IDatabaseContext databaseContext)
+		{
+			_databaseContext = databaseContext;
+
+		}
+		public async Task<ResultDto<List<GetNotificationResultDto>>> GetAllNotifications(GetNotificationDto input, int userId)
+		{
+			try
+			{
+				var result = await _databaseContext.Notifications
+												   .Where(n => n.ReceiverId == userId)
+												   .Select(n => new GetNotificationResultDto
+												   {
+													   NotificationId = n.Id,
+													   Message = n.Message,
+													   CreateDate = n.InsertTime,
+												   }).ToListAsync();
+
+				return new ResultDto<List<GetNotificationResultDto>>()
+				{
+					Data = result,
+					IsSuccess = true,
+					Message = "Success",
+				};
+			}
+			catch(Exception ex)
+			{
+				return new ResultDto<List<GetNotificationResultDto>>
+				{
+					Data = new(),
+					IsSuccess = false,
+					Message = ex.Message
+				};
+			}
+		}
+	}
+
+}
