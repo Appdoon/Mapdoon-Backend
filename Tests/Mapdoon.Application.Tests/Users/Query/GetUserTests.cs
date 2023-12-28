ed9d39b6733569fda4a0ba999e19425900cb3eb6
Arman <Armansinaie1380@gmail.com>
2023-12-29 02:28:05 +0330

    feat: add some test for user profile

---
+++ Tests/Mapdoon.Application.Tests/Users/Query/GetUserTests.cs
@@ -0,0 +1,27 @@
+﻿using Appdoon.Application.Services.Users.Query.GetUserService;
+using Appdoon.Domain.Entities.Users;
+using FluentAssertions;
+
+namespace Mapdoon.Application.Tests.Users.Query
+{
+    using static Testing;
+    public class GetUserTests : TestBase
+    {
+        [Test]
+        public async Task ShouldGetUserInfo()
+        {
+            var userId = AddEntity(new User
+            {
+                Email = "test@gmail.com",
+                Username = "Test",
+                Password = "password",
+            });
+
+            var result = await new GetUserService(GetDatabaseContext(), GetFacadeFileHandler()).Execute(userId);
+            result.IsSuccess.Should().Be(true);
+
+            result.Data.Email.Should().Be("test@gmail.com");
+            result.Data.Username.Should().Be("Test");
+        }
+    }
+}
