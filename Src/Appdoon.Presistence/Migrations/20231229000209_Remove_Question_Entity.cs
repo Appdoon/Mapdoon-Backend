908b2c73689e2d26ac52adddbe8c793393c937cf
Arman <Armansinaie1380@gmail.com>
2023-12-29 03:34:06 +0330

    refactor: remove question entity

---
+++ Src/Appdoon.Presistence/Migrations/20231229000209_Remove_Question_Entity.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mapdoon.Presistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Question_Entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5772));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5906));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5915));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Answer = table.Column<int>(type: "int", nullable: false),
+                    InsertTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    Option1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option3 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option4 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QuestionDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 28, 19, 44, 15, 637, DateTimeKind.Local).AddTicks(2940));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 28, 19, 44, 15, 637, DateTimeKind.Local).AddTicks(3017));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 28, 19, 44, 15, 637, DateTimeKind.Local).AddTicks(3028));
+        }
+    }
+}
