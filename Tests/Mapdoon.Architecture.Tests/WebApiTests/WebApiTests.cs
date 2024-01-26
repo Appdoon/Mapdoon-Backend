b850cd31ee5bf3c62a691c45f21c59da048a331f
MrSekiro <ab.arshia@gmail.com>
2024-01-26 16:31:08 +0330

    test: add architecure tests for Domain and WebApi

---
+++ Tests/Mapdoon.Architecture.Tests/WebApiTests/WebApiTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NetArchTest.Rules;
+
+namespace Mapdoon.Architecture.Tests.WebApiTests
+{
+	public class WebApiTests : BaseTest
+	{
+		[Fact]
+		public void ControllerClassesShouldInheritFromController()
+		{
+			var result = Types.InAssembly(WebApiAssembly)
+										  .That()
+										  .ResideInNamespaceContaining("Mapdoon.WebApi.Controllers")
+										  .Should()
+										  .Inherit(typeof(ControllerBase))
+										  .GetResult();
+
+			result.IsSuccessful.Should().BeTrue();
+		}
+
+		[Fact]
+		public void ControllerClassesNameShouldEndWithController()
+		{
+			var result = Types.InAssembly(WebApiAssembly)
+										  .That()
+										  .ResideInNamespaceContaining("Mapdoon.WebApi.Controllers")
+										  .Should()
+										  .HaveNameEndingWith("Controller")
+										  .GetResult();
+
+			result.IsSuccessful.Should().BeTrue();
+		}
+	}
+}
