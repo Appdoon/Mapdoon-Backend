feceb8f63154070959b1ce89d45897d44b517398
Arman <Armansinaie1380@gmail.com>
2024-01-29 11:08:04 +0330

    fix: rest notfication server before run any test

--- Tests/Mapdoon.Application.Tests/TestBase.cs
+++ Tests/Mapdoon.Application.Tests/TestBase.cs
@@ -10,7 +10,7 @@ namespace Mapdoon.Application.Tests
             await ResetStateAsync();
             ResetDatabaseContext();
             ResetFacadeFileHandler();
-            // ResetNotificationService();
+            ResetNotificationService();
         }
     }
 }
