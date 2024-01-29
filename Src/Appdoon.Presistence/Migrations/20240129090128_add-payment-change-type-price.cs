99a1b6cc1c0e73f90ddfbdcddc12721a0c1c0edf
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2024-01-29 12:34:31 +0330

    fix: fix migrations

---
+++ Src/Appdoon.Presistence/Migrations/20240129090128_add-payment-change-type-price.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mapdoon.Presistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addpaymentchangetypeprice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "RoadMaps",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoadmapId = table.Column<int>(type: "int", nullable: false),
+                    IsFinaly = table.Column<bool>(type: "bit", nullable: false),
+                    InsertTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_RoadMaps_RoadmapId",
+                        column: x => x.RoadmapId,
+                        principalTable: "RoadMaps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Payments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2024, 1, 29, 12, 31, 28, 58, DateTimeKind.Local).AddTicks(7324));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2024, 1, 29, 12, 31, 28, 58, DateTimeKind.Local).AddTicks(7387));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2024, 1, 29, 12, 31, 28, 58, DateTimeKind.Local).AddTicks(7398));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_RoadmapId",
+                table: "Payments",
+                column: "RoadmapId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_UserId",
+                table: "Payments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "RoadMaps",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5772));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5906));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertTime",
+                value: new DateTime(2023, 12, 29, 3, 32, 9, 40, DateTimeKind.Local).AddTicks(5915));
+        }
+    }
+}
